996ab12617552910615808b2787219b784c5d796
kwambokanaomie <mogakanaomy@gmail.com>
2019-11-03 19:27:33 -0800

    first commit

---
+++ exercise1.py
@@ -0,0 +1,19 @@
+
+name= "Stan"
+age= "20"
+print ("Hello World")
+print("my name is " + name)
+print("i am " + age + " + years of age, young, right? ")
+mylist=[12,4,56,17, 8, 99]
+print("The maximum number in this list: ", (mylist),"is", max(mylist),".")
+mean=(12+4+56+17+8+99)/6
+print("The mean: ", (mylist), "is", (mean))
+a="apple"
+b="boy"
+c="cow"
+z="zebra"
+print("A for " + a)
+print("B for " + b)
+print("C for " + c)
+print("...")
+print("Z for " + z)
